b1657430bc3c899fa8cef3a6aedc389d1234fd8b
caballo <imanol.l.p@hotmail.com>
2021-10-27 17:19:28 +0200

    Added temporary loadscreen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React, { useState,  } from 'react';
+import React, { useState } from 'react';
 import GetCats from './Components/GetCats';
+import Loadscreen from './Components/Loadscreen';
 
 function App() {
 	const [data, setData] = useState('');
+	const [isLoading, setIsLoading] = useState(true);
 
-	const getCats = (catData) => setData(catData);
+	const logData = () => console.log(data);
+	const getCats = (catData) => {
+		setData(catData);
+		setIsLoading(false);
+	};
+	const loadscreen = isLoading ? <Loadscreen /> : null;
 
 	return (
 		<div>
 			<GetCats callback={getCats} />
+			{loadscreen}
+			<button onClick={logData}>Log Data</button>
 		</div>
 	);
 }
---
+++ src/Components/Card.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react'
+
+export default function Card(props) {
+    const [isClicked, setIsClicked] = useState(false)
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
